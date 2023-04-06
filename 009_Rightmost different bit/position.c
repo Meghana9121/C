d2740a79ce0b2553a01e3d70d473b810c3e7f31d
meghana <92004419+Meghana9121@users.noreply.github.com>
2023-04-06 21:10:17 +0530

    Update position.c

--- 009_Rightmost different bit/position.c
+++ 009_Rightmost different bit/position.c
@@ -1,21 +1,29 @@
 #include<stdio.h>
 #include<math.h>
 #include <strings.h>
-int posOfRightMostDiffBit(int m, int n){
-int res;
-printf("Enter the numbers:");
-scanf("%d %d",&m,&n);
-res=m^n;
-if(res==0)
-   return 0;
-return log2(res & -res)+1;
-//return ffs(res);
+int posOfRightMostDiffBit(int m, int n)
+{
+   int res;
+	printf("Enter the numbers:");
+	scanf("%d %d",&m,&n);
+	res=m^n;
+	double log2(double x)
+	 {
+    		return log(x) / log(2.0);
+	 }
+	if(res==0)
+   		return 0;
+   		//return (log10(res & -res) / log10(2))+1;
+
+	return log2(res & -res)+1;
+	//return ffs(res);
 }
 
-int main(){
+int main()
+{
 unsigned int m,n;
 printf("Position of rightmost different bit:%d",posOfRightMostDiffBit(m,n));
 }
-//while compiling link it with -lme like
+//while compiling link it with -lm like
 //gcc position.c -lm
 
