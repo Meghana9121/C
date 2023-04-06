2e0e7d487127924b6c5c8b5a9757da2a7979f0cc
meghana <92004419+Meghana9121@users.noreply.github.com>
2023-04-06 19:57:55 +0530

    Update message.c

--- 008_Message Spreading/message.c
+++ 008_Message Spreading/message.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
-int main(){
+int spread(int N){
+  return 2*N-2;
+}
+int main()
+{
 int N;
 printf("Enter the num:");
 scanf("%d",&N);
-printf("A total of %d messages are sent \n",(2*N-2));
+printf("A total of %d messages are sent \n",spread(N));
 }
