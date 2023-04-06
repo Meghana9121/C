7f3438bab474898e72a8af683a704343dac0220f
meghana <92004419+Meghana9121@users.noreply.github.com>
2023-04-06 20:36:57 +0530

    Update multiples_of_5.c

--- 006_Anshuman's Favourite Number/multiples_of_5.c
+++ 006_Anshuman's Favourite Number/multiples_of_5.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
-int main(){
+long int check(long int N)
+{
+  if(N%5==0){
+	printf("YES\n");
+	printf("Because %ld can be written as a sum or difference of 5's",N);
+      }
+  else{
+	printf("NO\n");
+	printf("%ld can not be written in sum or difference of 5's.",N);
+      }
+}
+int main()
+{
 long int N;
 printf("Enter the number:");
-scanf("%d",&N);
-if(N%5==0){
-printf("YES\n");
-printf("Because %d can be written as a sum or difference of 5's",N);
+scanf("%ld",&N);
+check(N);
 }
-else{
-printf("NO\n");
-printf("%d can not be written in sum or difference of 5's.",N);
-}
-}
-
